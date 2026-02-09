7b451b3096326bbdaa2e92948b1dd22df43a0400
aw-andre <aw.andregerard@gmail.com>
2026-02-09 12:32:54 -0500

    feat: basic working sweep pro

--- flake.nix
+++ flake.nix
@@ -8,35 +8,58 @@
     };
   };
 
-  outputs = { self, nixpkgs, zmk-nix }: let
-    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
-  in {
-    packages = forAllSystems (system: rec {
-      default = firmware;
+  outputs = { self, nixpkgs, zmk-nix }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      forAllSystems =
+        nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+    in {
+      packages = forAllSystems (system: rec {
+        default = firmware;
 
-      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
-        name = "firmware";
+        firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+          name = "firmware";
 
-        src = nixpkgs.lib.sourceFilesBySuffices self [ ".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig" ];
+          nativeBuildInputs = with pkgs; [
+            protobuf
+            python3Packages.grpcio-tools
+          ];
 
-        board = "nice_nano@2.0.0";
-        shield = "lily58_%PART%";
+          src = nixpkgs.lib.sourceFilesBySuffices self [
+            ".board"
+            ".cmake"
+            ".conf"
+            ".defconfig"
+            ".dts"
+            ".dtsi"
+            ".json"
+            ".keymap"
+            ".overlay"
+            ".shield"
+            ".yml"
+            "_defconfig"
+          ];
 
-        zephyrDepsHash = "sha256-q1q27JpDgccOHuh9C1ZDwm7mnXZmG+Rs5qcWT+HFy9U=";
+          board = "nice_nano@2.0.0";
+          shield = "sweep_%PART%";
 
-        meta = {
-          description = "ZMK firmware";
-          license = nixpkgs.lib.licenses.mit;
-          platforms = nixpkgs.lib.platforms.all;
+          enableZmkStudio = true;
+
+          zephyrDepsHash =
+            "sha256-sEC1BhOdN00NxEWdyugxdQcuqzM5aY6W0EE5kA0orA4=";
+
+          meta = {
+            description = "ZMK firmware";
+            license = nixpkgs.lib.licenses.mit;
+            platforms = nixpkgs.lib.platforms.all;
+          };
         };
-      };
 
-      flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
-      update = zmk-nix.packages.${system}.update;
-    });
+        flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+        update = zmk-nix.packages.${system}.update;
+      });
 
-    devShells = forAllSystems (system: {
-      default = zmk-nix.devShells.${system}.default;
-    });
-  };
+      devShells = forAllSystems
+        (system: { default = zmk-nix.devShells.${system}.default; });
+    };
 }
